df3b3f4b516af1c0550f46a5c5ecda4f99b82b85
gyozasukisuki <80611764+gyozasukisuki@users.noreply.github.com>
2022-07-03 18:32:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -42,6 +42,12 @@ function sleep(waitSec, callbackFunc) {
 function checkMassage(name){
     if(name === "SS"){
         return "<b>お姉ちゃんへ</b><br>もうお姉ちゃんは今日で16歳なんだね。16年って長いの? 短いの?<br class='changeline'> 僕にはまだわかりません。<br>今日に至るまで、<br class='changeline'>沢山の人に支えられてきたと思う。<br class='changeline'>その中には、<br class='changeline'>いとこも、友達もいたと思う。<br>でも、その分お姉ちゃんもたくさんの人を支えてきたんじゃないかなと思うよ。<br>このサイトを作るだけでも<br class='changeline'>たくさんの人の力を借りました。<br class='changeline'>もちろんパパやママもだけれど、HTMLの書き方を載せてくれているサイトを作った人にも支えてもらいました。<br>お姉ちゃんにも<br class='changeline'>たくさん支えてもらいました。<br>お姉ちゃんは本当に<b style='color:red'>心が広い</b>と感じるよ。尊敬してます...<br>お姉ちゃんは<br class='changeline'>これから何に興味を持つのかなぁ<br>まぁとりあえずテストは頑張ってねーー<br>おたんじょうびおめでとう!<br>by テスト後のS.S";
+    }else if(name === "BB_and_JJ"){
+        return "Wちゃん<br>お誕生日おめでとう。<br class='changeline'>大きくなりましたね。<br>この先、どんな大人になっていくのか<br class='changeline'>楽しみと同時に<br class='changeline'>ジジババは<b>いつも見守っていますよ。</b>";
+    }else if(name === "OK"){
+        return "誕生日おめでとう😁<br>勉強教えてください。";
+    }else if(name === "MaMa"){
+        return "ママは、お姉ちゃんが、<br class='changeline'><b>元気</b>に<b>楽しく</b>毎日過ごしてくれたら<br class='changeline'>うれしいよ。";
     }
 }
 
